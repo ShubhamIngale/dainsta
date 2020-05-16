db034468f18c823b9c0fa9f847ed6cdbe190647b
Shubham Ingale <shubhamingale@Shubhams-MacBook-Pro.local>
2020-05-16 16:27:12 +0530

    tax and payment tab done

--- js/supplier/profile-info.js
+++ js/supplier/profile-info.js
@@ -1,3 +1,10 @@
 $('#skipBtn').on('click', function (e) {
     $('.nav-tabs > .active').next('li').find('a').trigger('click');
 });
+
+$('#skipBtnTwo').on('click', function (e) {
+    $('.nav-tabs > .active').next('li').find('a').trigger('click');
+});
+
+
+// apply dynamic active to nav tablink
