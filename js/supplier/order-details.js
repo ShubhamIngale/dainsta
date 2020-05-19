5980cfa59f2a1a1f85372fd19dc11f994a3293d1
Shubham Ingale <shubhamingale@Shubhams-MacBook-Pro.local>
2020-05-20 00:58:09 +0530

    order dtails complete

--- js/supplier/order-details.js
+++ js/supplier/order-details.js
@@ -8,11 +8,13 @@ cols.forEach((col, index) => {
         var content = this.nextElementSibling;
         const icon = this.querySelector("#icon");
         const toggle = cols[index].querySelector('.toggle');
+        const checkGroup = document.querySelector('.checkbox-group');
         if (content.style.maxHeight) {
           content.style.maxHeight = null;
           icon.style.setProperty("--height", "70px");
           icon.classList.remove('active-'+[index+1]);
           toggle.classList.remove('toggle-up');
+          checkGroup.style.visibility = 'hidden';
         } else {
           content.style.maxHeight = content.scrollHeight + "px";
           lh = 100;
@@ -20,10 +22,11 @@ cols.forEach((col, index) => {
           icon.style.setProperty("--height", lineHeight + "px");
           icon.classList.add('active-'+[index+1]);
           toggle.classList.add('toggle-up'); 
+          checkGroup.style.visibility = 'visible';
         }
     }
 
-    if(index >0 && index <3) {
+    if(index ==1) {
         var content = this.nextElementSibling;
         const icon = this.querySelector("#icon");
         const toggle = cols[index].querySelector('.toggle');
@@ -43,6 +46,29 @@ cols.forEach((col, index) => {
          toggle.classList.add('toggle-up')    
         }
     }
+    if(index ==2) {
+        var content = this.nextElementSibling;
+        const icon = this.querySelector("#icon");
+        const toggle = cols[index].querySelector('.toggle');
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+          icon.style.setProperty("--height", "70px");
+          icon.classList.remove('active-'+[index+1]);
+          toggle.classList.remove('toggle-up')    
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+          lh = 100;
+          const lineHeight = content.scrollHeight + lh;
+          icon.style.setProperty("--height", lineHeight + "px");
+          icon.classList.add('active-'+[index+1]);
+          const prevIcon1 = cols[index-1].querySelector('#icon');
+          const prevIcon2 = cols[index-2].querySelector('#icon');
+         prevIcon1.classList.add('done-'+[index]);
+         prevIcon2.classList.add('done-'+[index]);
+         toggle.classList.add('toggle-up')    
+        }
+    }
+
 
     if(index == 3) {
         var content = this.nextElementSibling;
@@ -55,8 +81,12 @@ cols.forEach((col, index) => {
         } else {
           content.style.maxHeight = content.scrollHeight + "px";
           icon.classList.add('active-'+[index+1]);
-          const prevIcon = cols[index-1].querySelector('#icon');
-          prevIcon.classList.add('done-'+[index]);
+          const prevIcon1 = cols[index-1].querySelector('#icon');
+          const prevIcon2 = cols[index-2].querySelector('#icon');
+          const prevIcon3 = cols[index-3].querySelector('#icon');
+          prevIcon1.classList.add('done-'+[index]);
+          prevIcon2.classList.add('done-'+[index]);
+          prevIcon3.classList.add('done-'+[index]);
           toggle.classList.add('toggle-up')    
         }
     }
