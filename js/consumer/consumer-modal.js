a002c1556429dabaefd8426d4202dde49c968b8d
Shubham Ingale <shubhamingale@Shubhams-MacBook-Pro.local>
2020-05-24 09:57:03 +0530

    review page added

--- js/consumer/consumer-modal.js
+++ js/consumer/consumer-modal.js
@@ -35,3 +35,17 @@ function bootstrapTabControl(){
     });
   }
   bootstrapTabControl();
+
+
+
+
+//   vanilla js for review
+
+const starEls = document.querySelectorAll('.star.rating');
+starEls.forEach(star => {
+  star.addEventListener('click', function(e) {
+    let starEl = e.currentTarget;
+    console.log(starEl.parentNode.dataset.stars + ", " + starEl.dataset.rating);
+    starEl.parentNode.setAttribute('data-stars', starEl.dataset.rating);
+  });
+})
